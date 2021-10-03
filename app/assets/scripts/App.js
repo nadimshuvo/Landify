c71adbbc6cf6e12340307375b4039c9b7d04ee2e
nadimshuvo <nadimshuvo1997@gmail.com>
2021-10-03 07:27:56 +0600

    ALhamdulillah

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,10 @@
 import '../styles/styles.css'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".r75"), 75)
+new RevealOnScroll(document.querySelectorAll(".r60"), 60)
+new RevealOnScroll(document.querySelectorAll(".r50"), 50)
+new RevealOnScroll(document.querySelectorAll(".r30"), 30)
 
 if (module.hot) {
     module.hot.accept()
