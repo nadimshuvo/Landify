65919dedc6b7c37ade22314927c729939fa83eed
nadimshuvo <nadimshuvo1997@gmail.com>
2021-10-13 20:01:00 +0600

    Alhamdulillah

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 
+new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".r75"), 75)
 new RevealOnScroll(document.querySelectorAll(".r60"), 60)
 new RevealOnScroll(document.querySelectorAll(".r50"), 50)
