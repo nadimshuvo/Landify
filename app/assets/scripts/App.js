a3b40ca189262e8c32b7b2680b3156b3b54cc94d
nadimshuvo <nadimshuvo1997@gmail.com>
2021-10-14 19:50:50 +0600

    Alhamdulillah 2nd Project Completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,6 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
-import RevealOnScroll from './modules/RevealOnScroll'
-
 new MobileMenu();
-new RevealOnScroll(document.querySelectorAll(".r75"), 75)
-new RevealOnScroll(document.querySelectorAll(".r60"), 60)
-new RevealOnScroll(document.querySelectorAll(".r50"), 50)
-new RevealOnScroll(document.querySelectorAll(".r30"), 30)
 
 if (module.hot) {
     module.hot.accept()
